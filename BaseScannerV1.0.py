daab7c12a1b780c6780a8af429dd5c84cc02a2d1
Ruben Kharel <39809189+rubenkharel@users.noreply.github.com>
2019-11-08 19:59:05 +0545

    Beta-version final commit

--- BaseScannerV1.0.py
+++ BaseScannerV1.0.py
@@ -3,7 +3,8 @@ from colorama import Fore
 from colorama import Style
 from bs4 import BeautifulSoup
 
-print(F'''{Fore.GREEN}
+
+print(F'''{Fore.MAGENTA}
   ____                    _____                                 
  |  _ \                  / ____|                                
  | |_) | __ _ ___  ___  | (___   ___ __ _ _ __  _ __   ___ _ __ 
@@ -17,11 +18,10 @@ print(F'''{Fore.GREEN}
 headers = requests.utils.default_headers()
 headers.update(
     {
-        'User-Agent': 'My kera jasto Agent 99999.99',
-        'From': 'lado@pornhub.com'
+        'User-Agent': 'xSphere / CYBERACE 1.0',
+        'From': 'none'
     }
 )
-
 i = 1
 while i > 0:
     url = input("Enter the url: ").lower()
@@ -33,35 +33,52 @@ while i > 0:
         i = -1
         print("URL acceptable")
 
+def check_if_wp(urlx):
+    """This function checks the version of
+    the wordpress site"""
+
+    if ((urlx[-1]) == "/"):
+        urlz = "readme.html"
+    else:
+        urlz = "/readme.html"
+    urlreadme = urlx + urlz
+
+    response = requests.get(urlreadme.format(), headers=headers)
+    ver_soup = BeautifulSoup(response.content, 'html.parser')
+    if ver_soup.title.string == 'WordPress › ReadMe':
+        WPsite = True
+    else:
+        WPsite = False
+    return WPsite
 
 def check_version_feed(urlx):
-    '''This function checks the version of
-    the wordpress site'''
+    """This function checks the version of
+    the wordpress site"""
 
-    print("Checking version......")
     if ((urlx[-1]) == "/"):
         urly = "feed"
     else:
         urly = "/feed"
-    urlformated = urlx + urly
-    response = requests.get(urlformated, headers=headers)
-    if response.status_code == 200:
+    urlfeed = urlx + urly
+
+    if check_if_wp(urlx) == True:
+        response = requests.get(urlfeed, headers=headers)
         ver_soup = BeautifulSoup(response.content, 'html.parser')
         ver_fin = ver_soup.generator.string
-        print(f'{Fore.GREEN}Wordpress version Detected: {Style.RESET_ALL}' + ver_fin[-5:])
 
-        rss = requests.get(urlformated)
+        print(f'{Fore.GREEN}Wordpress version Detected: {Style.RESET_ALL}' + ver_fin[-5:])
+        rss = requests.get(urlfeed)
         if 'Response' in rss:
             print('rss not founnd')
         else:
-            print(f'{Fore.GREEN}Rss Feed Found: {Style.RESET_ALL}' + urlformated)
-    else:
-        print(f'{Fore.RED}Not valid Wordpress site : {Style.RESET_ALL}' + urlformated)
+            print(f'{Fore.GREEN}Rss Feed Found: {Style.RESET_ALL}' + urlfeed)
 
+    else:
+        print("errorxxxxxxxxxx")
 
 def adminpan(urlx):
-    '''This function checks if a site
-    have admin panel or not'''
+    """This function checks if a site
+    have admin panel or not"""
 
     if ((urlx[-1]) == "/"):
         urly = "wp-admin"
@@ -77,8 +94,8 @@ def adminpan(urlx):
 
 
 def xmrpc_check(urlx):
-    '''This function checks if a site is
-    vulnerable to bruteforce attack or not'''
+    """This function checks if a site is
+    vulnerable to bruteforce attack or not"""
 
     if ((urlx[-1]) == "/"):
         urly = "xmlrpc.php"
@@ -93,14 +110,16 @@ def xmrpc_check(urlx):
 
 
 def userfinder(urlx):
-    '''This function just extracts a json
-    data and find users from the data'''
+    """This function just extracts a json
+    data and find users from the data"""
 
     if ((urlx[-1]) == "/"):
         urly = "wp-json/wp/v2/users"
     else:
         urly = "/wp-json/wp/v2/users"
     url = urlx + urly
+
+    #https://dinace.me/wp-json/wp/v2/users
 
     print("Finding users.....")
     r = requests.get(url, headers=headers)
@@ -109,15 +128,50 @@ def userfinder(urlx):
         print(f'{Fore.GREEN}Users found\n--------------------- {Style.RESET_ALL}')
         for i in range(0, len(JsonAll)):
             j = i + 1
-            print(str(j), ". ", JsonAll[i]['name'])
+            print(str(j), ". ", JsonAll[i]['slug'])
     else:
         print(f'{Fore.RED}Fuck!!! We got a error {Style.RESET_ALL}')
         print(f'{Fore.RED}Status Code: {Style.RESET_ALL}', r.status_code)
 
+def naked_url(urlx):
+    """This function cuts the url into naked format
+    without http:// or https://"""
+    url=''
+    if 'http://' == urlx[:7]:
+        url = urlx[7:]
+    elif "https://" == urlx[:8]:
+        url = urlx[8:]
+    return url
 
-check_version_feed(url)
-adminpan(url)
-xmrpc_check(url)
-userfinder(url)
+def dns_finder(urlx):
+    """This function is used to extract the name server of the domain"""
+    print("Scanning for subdomains ........\n")
+    url_naked = naked_url(urlx)
+    full_url = ("https://certspotter.com/api/v0/certs?domain=" +url_naked)
+    suber = requests.get(full_url, verify=requests.utils.DEFAULT_CA_BUNDLE_PATH, headers=headers)
+    JsonAll = suber.json()
+    for i in range(0, len(JsonAll)):
+        j = i + 1
+        print(str(j), ". ", JsonAll[i]['dns_names'])
+    print('')
 
-# https://english.onlinekhabar.com
+def pluginscan(urlx):
+    print("\nSearching For Installed Themes & Plugins.........\n")
+    theme = requests.get(url,headers=headers)
+    theme_soup = BeautifulSoup(theme.content,'html.parser')
+    print(f"{Fore.GREEN}Found themes and plugins:{Style.RESET_ALL}")
+    i = 1
+    for link in theme_soup.find_all(media="all", href=True):
+        print(i," . "+ link['href'])
+        i = i+1
+
+if check_if_wp(url) == True:
+    print(f'{Fore.BLUE}It is WordPress enabled site {Style.RESET_ALL}')
+    check_version_feed(url)
+    adminpan(url)
+    xmrpc_check(url)
+    userfinder(url)
+    dns_finder(url)
+    pluginscan(url)
+else:
+    print(f'{Fore.RED}It is not a WordPress enabled site {Style.RESET_ALL}')
